18df0f5e14c5fedfeb3c7e6b5542a5f4fea890c4
MADROCHA <madlarocha@gmail.com>
2022-07-24 01:22:17 +0200

    about.blade_quienesSomos_sección_wip_responsive_design

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,117 +3,187 @@
 @section('content')
 <div class="">
 
-    <section class="bg-gradient-blue justify-center w-full h-auto border-4 min-h-screen text-3xl leading-snug | | p-[px] gap-[30px]  bg-gradient-to-b from-colorYellow to-colorTourq "  >
+<main class="w-full  justify-center font-sansita text-colorBlack border-colorGreen border  ||  ">
+
+    <section class="bg-gradient-blue justify-center w-full h-auto border-4 min-h-screen text-3xl leading-snug | | p-[px] gap-[30px]  bg-gradient-to-b from-gradient-lightBlue to-gradient-midBlue "  >
 
         <div class="flex-col border-4 border-colorPink ">
             <div class="flex justify-center text-6xl leading-normal underline underline-offset-[20px]  p-10">
-                <p>¿QUIÉNES SOMOS? </p>
+            <p>¿QUIÉNES SOMOS? </p>
             </div>
             <!-- quienes somos -->
-            <section class= "border flex-col flex gap-10" >
+            <section class= "border flex-col flex gap-10 p-5"  >
 
-                        <div class="md:flex gap-20 ">
-                            <img class='border-2 h-80  w-full      rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1658244410168-78923d5c9f34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80')"  />
-                            <div class="flex-col flex gap-10">
-                                    <p class="border px-20 self-center flex text-center ">
-                                    La piedra angular de este proyecto es la RRRECREACIÓN, con sus dos acepciones esta palabra resume muy bien la motivación principal de esta iniciativa. 
-                                    </p>
-                                    <img class='border-2 h-80  w-full     rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1658244410168-78923d5c9f34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80')"  />
+                        <div class="md:flex-row gap-20  flex flex-col-reverse ">
+
+                            <div class="gap-10 md:flex md:flex-col flex-col-reverse flex border border-colorYellow">
+                                <img class='border-2 h-80  w-full       rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1658244410168-78923d5c9f34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80')"  />
+                                <p class="border  mx-1  flex text-center ">
+                                Recrear es crear de nuevo algo, y también es divertir, alegrar o deleitar.
+                                </p>
+                            </div>
+                            
+                            <div class="flex-col flex gap-10 border">
+                                <p class="border m-5 self-center flex text-center ">
+                                La piedra angular de este proyecto es la RRRECREACIÓN, con sus dos acepciones esta palabra resume muy bien la motivación principal de esta iniciativa. 
+                                </p>
+                                <img class='border-2 h-80  w-full     rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1658244410168-78923d5c9f34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80')"  />
                             </div>
                         </div>
 
 
-                        <div class="flex">
+                        <div class="flex border-2 border-colorWhite">
                         </div>
+                        
 
-                        <div class="flex justify-end">
+                        <div class="flex justify-end border">
+                            <div class="border">
+
                             <p class="border mx-20 self-center flex text-center ">
-                            Recrear es crear de nuevo algo, y también es divertir, alegrar o deleitar. Por eso sin duda, comenzamos una aventura con la intención de RRRECREAR.
+                            Recrear es crear de nuevo algo, y también es divertir, alegrar o deleitar.
                             </p>
                             <img class='border-2 h-80  w-full rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
+                            </div>
+                            <p class="border mx-20 self-center flex text-center ">
+                            Por eso sin duda, comenzamos una aventura con la intención de RRRECREAR.
+                            </p>
                         </div>
 
-                        <div class="text-colorWhite">
+                    <div class="text-colorWhite">
                         dsa
-                        </div>
+                    </div>
             </section>                      
-                <!-- quienes somos -->
-                        <div class="text-colorWhite">
+            <!-- quienes somos -->
+                    <div class="text-colorWhite">
                         dsa
-                        </div>
-                <!-- regla de 3R -->
-            <section class="border-colorTourq border-4 gap-10 flex flex-col mb-10">
-
+                    </div>
                     <div class="flex flex-col gap-10">
                         <img class=' p-[90px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/HxH2pZVv/702A6390.jpg')"  />
                     </div>
+            <!-- regla de 3R -->
+            <section class="border-colorTourq p-5 border-4 gap-10 flex flex-col">
 
-                    <div class="gap-6 flex flex-col">
+
+                <div class="gap-6 flex flex-col">
                         <p class="text-5xl lead font-semibold text-center underline underline-offset-8">
-                            La regla de las tres erres: reducir, reutilizar y reciclar.
+                        La regla de las tres erres
+                        </p>
+                </div>
+
+                <div class="container flex-col flex items-center gap-8 pb-5 self-center border  text-center">
+            
+                    <p class="w-4/6 ">
+                    Usar el arte como instrumento y medio para transformar y crear.      
+                    </p>
+                    <p class="w-4/6 ">
+                    El cambio y la revolución desde una visión artística.    
+                    </p>
+                    <p class="w-4/6 ">
+                    El arte es capaz de dinamizar y transformar.    
+                    </p>
+                </div>
+
+                <div class="flex-col flex sm:tracking-[16px] tracking-[12px]   gap-10 ">
+
+                    <div class=" sm:flex-row flex flex-col-reverse  sm:items-start items-center  gap-10 border">
+                        <img class='border-2 sm:p-40 p-32 sm:w-1/2 w-1/4    rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
+                        <p class="text-center "> 
+                        reducir 
+                        </p>
+                    </div>
+                    <div class="  sm:flex-row flex-col flex sm:items-start items-center  gap-10 place-content-end border">
+                        <p class="text-center "> 
+                        reutilizar
+                        </p>
+                        <img class='border-2 sm:p-40 p-32 sm:w-1/2 w-1/4    rounded-r-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/tgFkGQCV/702A6409.jpg')"  />
+                    </div>
+
+                    <div class=" sm:flex-row flex flex-col-reverse  sm:items-start items-center  gap-10 border">
+                        <img class='border-2 sm:p-40 p-32 sm:w-1/2 w-1/4    rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
+                        <p class="text-center ">
+                            y reciclar
                         </p>
                     </div>
 
-                    <div class="flex gap-32">
+                </div>
 
-                    <div class="flex gap-10">
-                        <div class="flex-col flex gap-10">
-                            <p class="text-center "> 
-                                Desde la recreación queremos activar la búsqueda para dar nueva vida a muchos objetos, reutilizar con creatividad para reducir. 
-                            </p>
-                            <img class='p-40  rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
-                        </div>
 
-                        <img class='border-2 p-40  h-full  bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/tgFkGQCV/702A6409.jpg')"  />
-                    </div>
+                <div class="container flex-col flex items-center gap-8 pb-5 self-center border  text-center">
+                    <p  class=" w-4/6  ">
+                        Desde la recreación queremos activar la búsqueda para dar nueva vida a muchos objetos, reutilizar con creatividad para reducir.  
+                    </p>
+                </div>
 
-                    <div class="flex flex-col">
-                            <img class='border-2 p-40  rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
-                            <p class="text-center ">
-                            Usar el arte como instrumento y medio para transformar y crear. 
-                            </p>
-                    </div>
 
-                    </div>
 
             </section>
                 
-            </div>      
+        </div>      
 
-        </section> 
-
-
-
-        <!-- ---- -->
-        <section class="bg-gradient-blue justify-center w-full h-auto border-4 min-h-screen | p-[] gap-[30px] flex  bg-gradient-to-b from-colorTourq to-colorBlack  "  >
+    </section> 
 
 
-        <div class=" border-4 border-colorPink max-w-xl md:flex flex flex-col gap-20   container w-full    ">
-            <img class='border-2 h-80      rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1658244410168-78923d5c9f34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80')"  />
-            <img class='border-2 p-40  h-full  rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://images.unsplash.com/photo-1559304879-0bbc7c0314ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')"  />
-            <img class='border-2 p-40  h-full  rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('')"  />
+
+    <!-- ---- -->
+    <section class="p-20 justify-center text-center  w-full h-auto border-4 min-h-screen | text-xl font-extrabold gap-[30px] flex sm:flex sm:flex-row flex-col  bg-gradient-to-b from-gradient-midBlue to-gradient-darkerBlue "  >
+        
+    
+        <div class=" border-4 justify-between border-colorPink max-w-xl md:flex flex flex-col gap-20   container w-full    ">
+            <img class='border-2 sm:p-40 sm:w-auto w-full h-96   rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/TPcbcR3Q/702A6509.jpg')"  />
+
+        <div class=" flex flex-col gap-5">
+            <p class="">
+            Por amor al arte, el amor y el arte son compañeros de viaje, conceptos ligados de manera indisoluble. 
+            </p>
+            <p class="">
+            Gracias a ese amor a través de la recreación crear el camino a la diversión, la alegría y el deleite.
+            </p>
+        </div>
 
         </div>
 
         <div class="justify-center flex flex-col max-w-prose border text-2xl font-sansita text-center gap-y-2.5">
             <p class="text-[40px] font-extrabold">Bop & LiveMusic  </p>
             <p class="border">CatchPhrase </p>
-            <p class="">Enim urna fames convallis leo ultrices ut facilisis. Et eros donec sagittis mauris sed duis cras integer viverra. Eu sed consequat in commodo vitae, nunc quam suspendisse nunc. Mollis sed nulla blandit nullam suspendisse habitant. Pellentesque ipsum sollicitudin a eget ornare. Mi et sed adipiscing id neque pellentesque fermentum. Ullamcorper rhoncus purus, vitae nec, leo. Diam nunc commodo lacinia risus urna. Arcu urna, nunc varius cursus. Rhoncus id nulla lacus, nunc.  </p>
-            <p class="">El cambio y la revolución desde una visión artística. El arte es capaz de dinamizar y transformar. Desde una visión artística la creación es infinita, los límites desaparecen, las dificultades se convierten en reto, y cualquier objeto, situación o momento se puede convertir en un lienzo en blanco para recrearnos. 
-                    Por amor al arte, el amor y el arte son compañeros de viaje, conceptos ligados de manera indisoluble. Gracias a ese amor a través de la recreación crear el camino a la diversión, la alegría y el deleite. 
-                    Nace BOPART, ESPAI DE RRRECREACIÓN. Un espacio de encuentro, creación, conversación, diversión, donde el pensamiento es libre y los límites están fuera de lo binario, reglado e institucional. Un lugar donde recrearse y recrear desde la sensibilidad, el amor y el humor..  </p>
+            <p>
+            Nace BOPART, ESPAI DE RRRECREACIÓN. 
+            </p>
+            <p>
+            Un espacio de encuentro, creación, conversación, diversión, donde el pensamiento es libre y los límites están fuera de lo binario, reglado e institucional.           </p>
+            </p>
+            <p>
+            Un lugar donde recrearse y recrear desde la sensibilidad, el amor y el humor.. 
+            </p>
+        </div>
+
+        <div class=" border-4 justify-between border-colorPink max-w-xl md:flex flex flex-col gap-20   container w-full    ">
+            <p class="">
+            Desde una visión artística la creación es infinita, los límites desaparecen, las dificultades se convierten en reto, y cualquier objeto, situación o momento se puede convertir en un lienzo en blanco para recrearnos. 
+            </p>
+    <!--             <img class='border-2   flex  rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('')"  />
+    -->   
+        <img class='border-2 sm:p-40 sm:w-auto w-full h-96   rounded-l-[28px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/4xWZvqTV/702A6299.jpg')"  />
+
         </div>
 
 
 
 
-
-
+        
     </section>  
 
-    <div class="flex flex-col gap-10">
-        <img class=' p-[90px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/HxH2pZVv/702A6390.jpg')"  />
-    </div>
+            <div class="flex flex-col gap-10">
+                <img class=' p-[90px] bg-cover bg-center bg-no-repeat' style=" background-image: url('https://i.postimg.cc/HxH2pZVv/702A6390.jpg')"  />
+            </div>
+
+
+
+
+
+            
+
+            
+</main>
 
 
 </div>
