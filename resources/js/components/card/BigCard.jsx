0e771c808415afd13f53c89d1eb7a26ce3327d5d
Jhon <jhonv4sq.01@gmail.com>
2022-07-25 22:03:30 +0200

    update

--- resources/js/components/card/BigCard.jsx
+++ resources/js/components/card/BigCard.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { createRoot } from 'react-dom/client'
 
 const BigCard = () => {
-  return (
-    <div>
 
+  const [event, setEvent] = useState([])
+  const getEvents = async () => {
+      try {
+          const response = await axios.get('/api/getFirstEvents')
+          setEvent(response.data[0])
+      } catch(err) {
+          console.log(err.response.request.response)
+      }
+  }
+
+
+  useEffect(() => {
+      getEvents()
+  }, [])
+
+  return (
+    <div className='flex w-full md:w-5/12  flex-row shadow border-1 border-[#929191] rounded-lg'>
+      <div className={`w-full ${!event.image ? 'bg-colorBlack': ''} bg-cover bg-center bg-no-repeat rounded-l-lg`} style={{ backgroundImage: `url(`+event.image+`)` }}></div>
+      <div className='w-full card flex flex-col gap-3 font-sansita'>
+        <h1 className='w-full text-center font-bold text-base md:text-3xl'>{event.name}</h1>
+        <div>
+          <p className='w-full text-sm md:text-xl truncate_text'>{event.description} </p><a href='#' className='text-colorTourq text-sm md:text-xl'>Ver mas</a>
+        </div>
+      </div>
     </div>
   )
 }
