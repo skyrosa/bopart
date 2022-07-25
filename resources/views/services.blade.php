97e0bafd8c8bc87bc66858daca80aa4ace5ed8ac
MADROCHA <madlarocha@gmail.com>
2022-07-26 01:41:16 +0200

    services.jsx_viewCreated_wipRowImage/

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="">
-    <div id="" >
-        
-    </div>
+<div  class=" border-2 h-screen border-colorPink flex justify-center items-center   || bg-gradient-to-b from-colorYellow to-colorTourq">
+            <div class="flex border place-self-start flex-wrap w-full || h-1/6 sm:h-80  ">
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-full md: lg:" style=" background-image: url('https://i.postimg.cc/4xWZvqTV/702A6299.jpg')" ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/JhgLvX0h/702A6304.jpg')"  ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+            </div>                            
+                            
+            <!-- --Start_component -->
+            <section>
+            <!-- --Start_component -->
+
+
+    
+                <!-- --ContactInfo -->
+                <!-- --ContactInfo -->
+
+                    
+                
+
+                            
+                        
+            
+
+                    
+            <!-- --End_component -->
+            </section>
+            <!-- --End_component -->
+
+
 </div>
 @endsection
