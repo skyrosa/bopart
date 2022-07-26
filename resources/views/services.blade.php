abc4538f083c9c2e5542447e7760ee297f6599cc
MADROCHA <madlarocha@gmail.com>
2022-07-26 03:26:47 +0200

    services.jsx_TextImages_smallImages_responsive_wip.0.1B/

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div  class=" border-2  border-colorPink flex flex-col  items-center   || bg-gradient-to-b from-colorYellow to-colorTourq">
             <div class="flex border place-self-start flex-wrap w-full || h-1/6 sm:h-80  ">
-                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-full md: lg:" style=" background-image: url('https://i.postimg.cc/4xWZvqTV/702A6299.jpg')" ></div>
-                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/JhgLvX0h/702A6304.jpg')"  ></div>
-                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
-                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-3/12 lg:w-1/4 h-28 sm:h-full  md: lg:" style=" background-image: url('https://i.postimg.cc/4xWZvqTV/702A6299.jpg')" ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-3/12 lg:w-1/4 h-28 sm:h-full  md: lg:" style=" background-image: url('https://i.postimg.cc/JhgLvX0h/702A6304.jpg')"  ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-3/12 lg:w-1/4 h-28 sm:h-full  md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+                <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-3/12 lg:w-1/4 h-28 sm:h-full  md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
             </div>                            
                             
         <!-- --Start_component -->
