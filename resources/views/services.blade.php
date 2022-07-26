7ed140614e9619628e145cb10d64e208c64d9015
MADROCHA <madlarocha@gmail.com>
2022-07-26 15:19:48 +0200

    services.jsx_responsiveDesignFigma_line32-to-43_wip.0.10D/

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -29,9 +29,9 @@
 
                         
                     <div class="flex border  ">
-                        <div class="border border-colorWhite flex bg-cover bg-no-repeat bg-center | w-1/2 sm: md: lg: | h sm:  md: lg: |" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+                        <div class="border border-colorWhite flex bg-cover bg-no-repeat bg-center | w-3/5 sm: md: lg: | h sm:  md: lg: |" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
 
-                        <p class="flex border p-2.5 w-1/2 border-colorPink ">
+                        <p class="flex border p-2.5 w-2/3 border-colorPink ">
                         Organizamos actividades culturales de todo tipo con la RRRecreación por bandera. Súmate a nuestra oferta: micro teatro, monólogos, magia, acústicos, debates, conferencias, recitales de poesía, presentación de libros, talleres y coloquios.
                         </p>
                     </div>
@@ -40,7 +40,7 @@
                         <p class="flex border p-2.5  w-2/3 border-colorPink ">
                         Somos un agente muy activo en nuestro territorio más inmediato, nos sentimos muy orgullosos y orgullosas de decir que nuestro proyecto es MADE IN BON PASTOR.
                         </p>
-                        <div class="border border-colorWhite flex bg-cover bg-no-repeat bg-center | w-2/3 sm: md: lg: | h sm:  md: lg: |" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
+                        <div class="border border-colorWhite flex bg-cover bg-no-repeat bg-center | w-3/5 sm: md: lg: | h sm:  md: lg: |" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
 
                     </div>
 
