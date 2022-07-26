d77284f132f4460cc303cf1a32f4eb3fab583273
MADROCHA <madlarocha@gmail.com>
2022-07-26 03:21:38 +0200

    services.jsx_TextImages_Composition_responsive_wip.0B/

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div  class=" border-2 h-screen border-colorPink flex justify-center items-center   || bg-gradient-to-b from-colorYellow to-colorTourq">
+<div  class=" border-2  border-colorPink flex flex-col  items-center   || bg-gradient-to-b from-colorYellow to-colorTourq">
             <div class="flex border place-self-start flex-wrap w-full || h-1/6 sm:h-80  ">
                 <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-full md: lg:" style=" background-image: url('https://i.postimg.cc/4xWZvqTV/702A6299.jpg')" ></div>
                 <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/JhgLvX0h/702A6304.jpg')"  ></div>
@@ -9,27 +9,128 @@
                 <div class="flex bg-cover bg-no-repeat bg-center w-full sm:w-1/4 md:w-1/2 lg:w-1/4 h-2/3 sm:h-80   md: lg:" style=" background-image: url('https://i.postimg.cc/bNGQjcmN/702A6466.jpg')"  ></div>
             </div>                            
                             
-            <!-- --Start_component -->
-            <section>
-            <!-- --Start_component -->
+        <!-- --Start_component -->
+        <section class=" flex flex-col ">
+        <!-- --Start_component -->
+                <div class=" flex text-colorPink">
+                    QUE OFRECEMOS
+                </div>
+                <div>
+                    <p>
+                    Un proyecto donde la igualdad y la diversidad es nuestra realidad más cotidiana.
+                    </p>
+                    <p>
+                        Colaboramos de manera transversal con el tejito asociativo de la ciudad, en especial el de nuestro barrio.
+                    </p>
+                </div>
 
 
-    
-                <!-- --ContactInfo -->
-                <!-- --ContactInfo -->
+
 
                     
-                
+                    <div>
+                    <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                        <p>
+                        Organizamos actividades culturales de todo tipo con la RRRecreación por bandera. Súmate a nuestra oferta: micro teatro, monólogos, magia, acústicos, debates, conferencias, recitales de poesía, presentación de libros, talleres y coloquios.
+                        </p>
+                    </div>
 
-                            
-                        
-            
+                    <div>
+                        <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                        <p>
+                        Somos un agente muy activo en nuestro territorio más inmediato, nos sentimos muy orgullosos y orgullosas de decir que nuestro proyecto es MADE IN BON PASTOR.
+                        </p>
+                    </div>
+
+                    <div>
+                      <p>
+                        Si quieres sumarte o si necesitas una colaboración desde el BOPART tenemos infinidad de ideas creativas para la organización y dinamización de eventos. 
+                      </p>
+                      <p>
+                         Dentro y fuera de nuestro espacio de RRRECREACIÓN te ofrecemos un amplio abanico de actividades, eventos y talleres con la RRRecreación como eje principal:
+                      </p>
+                    </div>
+
+                    <div>
+                    <section>                         
+                        <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                        <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                        <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                        <div class="" style="background-image: url('/storage/pictures/banner.png');"></div>
+                    </section>
+
+                    <section>
+                            <p>
+                            BopArt 
+                            </p>
+                        <div>
+                            <p>
+                                *Acciones de dinamización comunitaria.</p>
+                            <p>
+                                *Actuaciones musicales, cómicas, de magia... 
+                            </p>
+                            <p>
+                                *Talleres de economía circular. 
+                            </p>
+                            <p>
+                                *Debates y conferencias. 
+                            </p>
+                            <p>
+                                *Presentación de libros y discos. 
+                            </p>
+                        </div>
+
+                        <div>
+                            <p>
+                            *Actividades en grupo. 
+                            </p>
+                            <p>
+                            *Talleres teatrales. 
+                            </p>
+                            <p>
+                            *Grandes juegos reunidos con dinamización en vivo. 
+                            </p>
+                            <p>
+                            *Talleres de artesanía. 
+                            </p>
+                            <p>
+                            *Mercado de artesanía y economía circular.                             
+                            </p>
+                        </div>
+
+                        <div>
+                            <p>
+                                *Exposiciones artísticas. 
+                            </p>
+                            <p>
+                                *Acciones de apoyo y dinamización al comercio de proximidad. 
+                            </p>
+                            <p>
+                                *Organización de verbenas. 
+                            </p>
+                            <p>
+                                *Asesoría cultural en comunidades digitales. 
+                            </p>
+                            <p>
+                                *Oferta de espacios de coworking. 
+                            </p>
+                        </div>
+
+                    </section>
+                </div>
+
+                <p>
+                    Todas estás acciones y actividades buscan la creación de una red fuerte y dinámica de nuestra comunidad. Nos encanta ser tribu y trabajar por ella.
+                </p>
 
                     
-            <!-- --End_component -->
-            </section>
-            <!-- --End_component -->
-
-
+                    
+        <!-- --End_component -->
+        </section>
+        <!-- --End_component -->
 </div>
+
 @endsection
+
+
+
